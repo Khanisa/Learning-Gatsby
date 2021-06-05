48ca835c84156125377e5d6efd3fb7c1dfedd44f
Khanisa <y_anisat@ymail.com>
2021-06-05 22:51:16 +0100

    made some changes

--- src/components/jobCard.js
+++ src/components/jobCard.js
@@ -2,23 +2,31 @@ import * as React from "react";
 import { Link } from "gatsby";
 
 
-const JobCard = ({ refNo, jobLink, jobTitle, jobType, jobLocation }) => (
-    <div className="jobCard">
-        <div className="jobCard__container">
-            <div className="jobCard__top">
-                <div className="jobCard__refNo">
-                    <p>{refNo}</p>
-                </div>
-                <Link className="jobCard__link" to={jobLink}>
-                    {jobTitle}
-                </Link>
-                <div className="jobCard__location">
-                    <span className="co_black">{jobType}</span> <span>in</span> <span className="co_primary">{jobLocation}</span>
+const JobCard = (props) => {
+    const Job = props.JobDeets.map(JobDeet => {
+        return <div className="jobCard" key={JobDeet.id}>
+            <div className="jobCard__container">
+                <div className="jobCard__top">
+                    <div className="jobCard__refNo">
+                        <p>{JobDeet.refNo}</p>
+                    </div>
+                    <Link className="jobCard__link" to={JobDeet.jobLink}>
+                        {JobDeet.jobTitle}
+                    </Link>
+                    <div className="jobCard__location">
+                        <span className="co_black">{JobDeet.jobType}</span> <span>in</span> <span className="co_primary">{JobDeet.jobLocation}</span>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-)
+        
+    })
+    return(
+        <div>
+            {Job}
+        </div>
+    )
+}
 
 
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,40 +1,57 @@
-import * as React from "react"
-// import { Link } from "gatsby"
-// import { StaticImage } from "gatsby-plugin-image"
+import * as React from "react";
 import Layout from "../components/layout";
 import Seo from "../components/seo";
 import Hero from "../components/hero";
 import JobCard from "../components/jobCard";
 
-const IndexPage = () => (
-  <Layout>
-    {/* <Seo title="Home" /> */}
-    <Seo title="Employment Agency in Zurich" />
-    <div className="homePage">
-      <Hero 
-        heroText="Recruitment Experts That Care." 
-        heroImage="https://iet.ch/uploads/transforms/4ff0c9e5feb5093c616e26b18b51f3ef/1090/IET-Home_6c0c164bd2b597ee32b68b8b5755bd2e.png"
-        heroSrcSet="https://iet.ch/uploads/transforms/4ff0c9e5feb5093c616e26b18b51f3ef/1090/IET-Home_616b2c32fb7bb9679ad560fc61635847.png 400w, https://iet.ch/uploads/transforms/4ff0c9e5feb5093c616e26b18b51f3ef/1090/IET-Home_5ae402c9837860cd6e4f65e304b3a5fa.png 800w, https://iet.ch/uploads/transforms/4ff0c9e5feb5093c616e26b18b51f3ef/1090/IET-Home_6c0c164bd2b597ee32b68b8b5755bd2e.png 1200w" />
-
-      <div className="homePage__jobs">
-          <div className="homePage__jobs--container">
-            <div className="homePage__jobs--headerText">
-              <h2 className="headerText">Find your ideal job today.</h2>
-            </div>
-            <div>
-              <JobCard 
-                refNo="7465"
-                jobLink="https://iet.ch/jobs/senior-business-analyst-data-management"
-                jobTitle="Senior Business Analyst - Data Management"
-                jobType="Contract"
-                jobLocation="Zurich"
-              />
-            </div>
-          </div>
-      </div>
-    </div>
+const IndexPage = () => {
+  const details = [
     
-  </Layout>
-)
+    {
+      refNo: "7535",
+      jobLink: "https://iet.ch/jobs/kundenberater-in-contact-center-80-100",
+      jobTitle: "Senior Communications Manager - Innovation & Culture change",
+      jobType: "Contract",
+      jobLocation: "Zürich-Altstetten",
+    },
+    {
+      refNo: "7577",
+      jobLink: "https://iet.ch/jobs/kundenberater-in-contact-center-80-100",
+      jobTitle: "Kundenberater/in - Contact Center (80-100%)",
+      jobType: "Contract",
+      jobLocation: "Zürich-Altstetten",
+    },
+    {
+      refNo: "7465",
+      jobLink: "https://iet.ch/jobs/senior-business-analyst-data-management",
+      jobTitle: "Senior Business Analyst - Data Management",
+      jobType: "Contract",
+      jobLocation: "Zurich",
+    },
+  ]
+
+  return (
+    <Layout>
+      {/* <Seo title="Home" /> */}
+      <Seo title="Employment Agency in Zurich" />
+      <div className="homePage">
+        <Hero 
+          heroText="Recruitment Experts That Care." 
+          heroImage="https://iet.ch/uploads/transforms/4ff0c9e5feb5093c616e26b18b51f3ef/1090/IET-Home_6c0c164bd2b597ee32b68b8b5755bd2e.png"
+          heroSrcSet="https://iet.ch/uploads/transforms/4ff0c9e5feb5093c616e26b18b51f3ef/1090/IET-Home_616b2c32fb7bb9679ad560fc61635847.png 400w, https://iet.ch/uploads/transforms/4ff0c9e5feb5093c616e26b18b51f3ef/1090/IET-Home_5ae402c9837860cd6e4f65e304b3a5fa.png 800w, https://iet.ch/uploads/transforms/4ff0c9e5feb5093c616e26b18b51f3ef/1090/IET-Home_6c0c164bd2b597ee32b68b8b5755bd2e.png 1200w" />
+  
+        <div className="homePage__jobs">
+            <div className="homePage__jobs--container">
+              <div className="homePage__jobs--headerText">
+                <h2 className="headerText">Find your ideal job today.</h2>
+              </div>
+              <JobCard JobDeets={details} />
+            </div>
+        </div>
+      </div>
+      
+    </Layout>
+  )
+}
 
 export default IndexPage
